005369cdb28be5c765ae0f914e7de8103eca60b3
Robert Kaemmer <robert.wieland@rwth-aachen.de>
2020-03-04 15:24:15 +0100

    Cleaned up basically functional code. Integrating unit tests now.

--- bs-codechallenge/Main.cs
+++ bs-codechallenge/Main.cs
@@ -8,19 +8,14 @@
 //		2020/03/01	R K			    Started development. Introduced Main() and csv path checking 
 //			                        function.
 //      2020/03/03  R K             Finished csv path checking and started with file parsing
-//      2020/03/04  R K             Continued with file parsing and started with SQL commands
+//      2020/03/04  R K             Continued with file parsing and did SQL insertion
 //
 //-------------------------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 using System.Data.SqlClient;
-//using Microsoft.VisualBasic.FileIO;
-
 using System.IO;
 
 namespace bs_codechallenge
@@ -32,18 +27,24 @@ namespace bs_codechallenge
     ///-------------------------------------------------------------------------------------------------
     class Program
     {
+        /// <summary> Contains all the information for the SQL connection. </summary>
         private static SqlConnectionStringBuilder sqlConString;
 
         ///-------------------------------------------------------------------------------------------------
-        /// <summary>   Main function. </summary>
+        /// <summary>   Main function for CSV to SQL migration. </summary>
         /// 
-        /// <remarks>   R K, 2020/03/01 </remarks>
+        /// <remarks>   
+        ///     Initializes the SQL connection string builder and the default CSV file. Checks then the 
+        ///     suitability of a given file or directory and starts the migration if applicable.
+        /// 
+        ///     R K, 2020/03/01
+        /// </remarks>
         ///
-        /// <param name="args">    Command line input for the function. </param>
+        /// <param name="args">     Command line input for the function. Can only be a CSV file path for now. 
+        ///                         </param>
         ///-------------------------------------------------------------------------------------------------
         public static void Main(string[] args)
         {
-            // Initialize SQL connection string
             sqlConString = new SqlConnectionStringBuilder
             {
                 DataSource = "localhost",
@@ -52,9 +53,7 @@ namespace bs_codechallenge
                 InitialCatalog = "MainDB"
             };
 
-            // Set default input path
-            String inputPath = @"../../test2.csv";
-
+            String inputPath = @"../../test.csv";
             if (args.Length > 1)
             {
                 inputPath = args[0];
@@ -120,21 +119,20 @@ namespace bs_codechallenge
         private static bool IsCsvFile(String path)
         {
             String fileEnding = path.Substring(path.Length - 3, 3);
-            if (fileEnding.ToLower() == "csv")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return fileEnding.ToLower() == "csv";
         }
 
 
         ///-------------------------------------------------------------------------------------------------
-        /// <summary>   XXX Converts a given CSV file into an SQL data table. </summary>
+        /// <summary>   Converts a given CSV file into an SQL data table. </summary>
         /// 
-        /// <remarks>   R K, 2020/03/03 </remarks>
+        /// <remarks>   
+        ///     Extracts the table name from the path, creates the data table on the SQL server if possible
+        ///     and then writes the records in the data table. Empty parts of the records are substituted
+        ///     with NULLs. If the data table could not be created, the method aborts.
+        /// 
+        ///     R K, 2020/03/03 
+        /// </remarks>
         /// 
         /// <param name="csvPath">    Path leading to the CSV file. </param>
         ///-----------------------------------------------  --------------------------------------------------
@@ -142,17 +140,17 @@ namespace bs_codechallenge
         {
             String tableName = csvPath.Split('/').ToArray().Last();
             tableName = tableName.Split('.')[0].ToUpper();
-            // Console.WriteLine("Name: " + tableName + "\r\n");
 
             using (StreamReader csvFile = new StreamReader(csvPath))
             {
                 String columnLine = csvFile.ReadLine();
-                String[] columns = columnLine.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                String[] columns = columnLine.Split(new char[] { ';' });
 
-                // Abort if data table cannot be created
                 if (!CreateSqlTable(tableName, columns))
                 {
-                    Console.WriteLine("ERROR: Could not create SQL data table.");
+                    Console.WriteLine("ERROR: Could not create SQL data table. " + 
+                    "A table '{0}' might already exist.", tableName);
+                    return;
                 }
 
                 while(!csvFile.EndOfStream)
@@ -169,7 +167,7 @@ namespace bs_codechallenge
 
                     if (!InsertRecord(tableName, columns, newRecord))
                     {
-                        Console.WriteLine("ERROR: Could not insert record {0}.", newRecord[0]);
+                        Console.WriteLine("ERROR: Could not insert record no. {0}.", newRecord[0]);
                     }
                 }
             }
@@ -177,13 +175,14 @@ namespace bs_codechallenge
 
 
         ///-------------------------------------------------------------------------------------------------
-        /// <summary>   XXX Creates an SQL data table based on the column descriptions given in 
+        /// <summary>   Creates an SQL data table based on the column descriptions given in 
         ///             <paramref name="columns"/>. </summary>
         /// 
         /// <remarks>   R K, 2020/03/03 </remarks>
         /// 
-        /// <param name="tableName">    Path leading to the CSV file. </param>
-        /// <param name="columns">    Path leading to the CSV file. </param>
+        /// <param name="tableName">    Name for the new data table. </param>
+        /// <param name="columns">      Array with column names and data types, separated by "$$". </param>
+        /// 
         /// <returns>   Returns <see langword="true"/> if table has been created successfully, returns 
         ///             <see langword="false"/> otherwise. </returns>
         ///-----------------------------------------------  --------------------------------------------------
@@ -203,8 +202,6 @@ namespace bs_codechallenge
                     sqlCmdText.Remove(sqlCmdText.Length - 2, 2);
                     sqlCmdText.Append(");");
 
-                    Console.WriteLine(sqlCmdText.ToString());
-
                     using (SqlCommand sqlCmd = new SqlCommand(sqlCmdText.ToString(), dbConnection))
                     {
                         sqlCmd.ExecuteNonQuery();
@@ -221,68 +218,57 @@ namespace bs_codechallenge
 
 
         ///-------------------------------------------------------------------------------------------------
-        /// <summary>   XXX. </summary>
+        /// <summary>   Inserts the record <paramref name="data"/> into the SQL table 
+        ///             <paramref name="tableName"/>. </summary>
         /// 
-        /// <remarks>   R K, 2020/03/03 </remarks>
+        /// <remarks>   
+        ///     Data for CHAR/VARCHAR or TEXT columns is surrounded by inverted commas.
+        ///     
+        ///     R K, 2020/03/03
+        /// </remarks>
         /// 
         /// <param name="data">    Path leading to the CSV file. </param>
         /// 
-        /// <returns>   Returns <see langword="true"/> if record has been inserted successfully, returns 
-        ///             <see langword="false"/> otherwise. </returns>
+        /// <param name="tableName">    Name for the new data table. </param>
+        /// <param name="columns">      Array with column names and data types, separated by "$$". </param>
+        /// <param name="data">         Array with record data. </param>
+        /// 
+        /// <returns>   Returns <see langword="true"/> if record has been inserted successfully into the table
+        ///             <paramref name="tableName"/>, returns <see langword="false"/> otherwise. </returns>
         ///-----------------------------------------------  --------------------------------------------------
         private static bool InsertRecord(String tableName, String[] columns, String[] data)
         {
-            //try
-            //{
-            using (SqlConnection dbConnection = new SqlConnection(sqlConString.ConnectionString))
+            try
             {
-                dbConnection.Open();
-
-                //Console.WriteLine("SET IDENTITY_INSERT " + tableName + " ON;");
-
-                //// Activate identity insert for table
-                //using (SqlCommand sqlCmd = new SqlCommand("SET IDENTITY_INSERT " + tableName + " ON;", dbConnection))
-                //{
-                //    sqlCmd.ExecuteNonQuery();
-                //}
-
-                StringBuilder sqlCmdText = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
-                //StringBuilder sqlCmdText = new StringBuilder("INSERT INTO " + tableName + " (");
-                //foreach (String column in columns)
-                //{
-                //    sqlCmdText.Append(column.Split(new String[] { "$$" }, StringSplitOptions.None).First() + ", ");
-                //}
-
-                //sqlCmdText.Remove(sqlCmdText.Length - 2, 2);
-                //sqlCmdText.Append(") VALUES (");
-
-                for (int i = 1; i < data.Length; i++)
+                using (SqlConnection dbConnection = new SqlConnection(sqlConString.ConnectionString))
                 {
-                    if (columns[i].ToUpper().Contains("CHAR"))
-                    {
-                        sqlCmdText.Append(@"'" + data[i] + @"', ");
-                    }
-                    else
-                    {
-                        sqlCmdText.Append(data[i] + ", ");
-                    }
-                    //sqlCmdText.Append(data[i] + ", ");
-                }
-                sqlCmdText.Remove(sqlCmdText.Length - 2, 2);
-                sqlCmdText.Append(");");
+                    dbConnection.Open();
 
-                Console.WriteLine(sqlCmdText.ToString());
+                    StringBuilder sqlCmdText = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
+                    for (int i = 1; i < data.Length; i++)
+                    {
+                        if (columns[i].ToUpper().Contains("CHAR") || columns[i].ToUpper().Contains("TEXT"))
+                        {
+                            sqlCmdText.Append("'" + data[i] + "', ");
+                        }
+                        else
+                        {
+                            sqlCmdText.Append(data[i] + ", ");
+                        }
+                    }
+                    sqlCmdText.Remove(sqlCmdText.Length - 2, 2);
+                    sqlCmdText.Append(");");
 
-                using (SqlCommand sqlCmd = new SqlCommand(sqlCmdText.ToString(), dbConnection))
-                {
-                    sqlCmd.ExecuteNonQuery();
+                    using (SqlCommand sqlCmd = new SqlCommand(sqlCmdText.ToString(), dbConnection))
+                    {
+                        sqlCmd.ExecuteNonQuery();
+                    }
                 }
             }
-            //}
-            //catch (SqlException)
-            //{
-            //    return false;
-            //}
+            catch (SqlException)
+            {
+                return false;
+            }
 
             return true;
         }
